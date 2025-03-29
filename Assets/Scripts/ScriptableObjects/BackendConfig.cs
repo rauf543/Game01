57eb25b08d3b200717913e88124d6e3406b3eefc
Unity Developer <dev@example.com>
2025-03-30 00:41:38 +0300

    feat(network): Add BackendConfig ScriptableObject for API URL configuration [CS-NET-03]

---
+++ Assets/Scripts/ScriptableObjects/BackendConfig.cs
@@ -0,0 +1,7 @@
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "BackendConfig", menuName = "Game/Backend Configuration")]
+public class BackendConfig : ScriptableObject
+{
+    public string baseUrl;
+}
